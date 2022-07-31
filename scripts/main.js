435a272fe953d894533df7b5541c57c0967d168f
Rémy Kies <gouru.rk.gouru.rk@gmail.com>
2022-08-01 01:37:44 +0200

    remove move if check complete

--- scripts/main.js
+++ scripts/main.js
@@ -13,22 +13,16 @@ const piecesArray = {
 let currentPlayer;
 
 // check if the king of the `color` is in check
-function isCheck(color, pieces=undefined) {
-    if (pieces == undefined) {
-        pieces = piecesArray;
-    }
+function isCheck(color) {
     // find the position of the king
-    let king = findPiecesByType(pieces[color], 'king')[0];
+    let king = findPiecesByType(piecesArray[color], 'king')[0];
     // find if the opponent pieces attack the king
-    return isSquareAttacked(king.getPos(), reverseColor[color], pieces);
+    return isSquareAttacked(king.getPos(), reverseColor[color]);
 }
 
 // check if the `square` is attacked by the `color`
-function isSquareAttacked(square, color, pieces=undefined) {
-    if (pieces == undefined) {
-        pieces = piecesArray;
-    }
-    pieces = pieces[color];
+function isSquareAttacked(square, color) {
+    let pieces = piecesArray[color];
     for (let piece of pieces) {
         let legalMoves = piece.getLegalMoves();
         if (isInsideArray(legalMoves, square)) {
--- scripts/moves.js
+++ scripts/moves.js
@@ -1,21 +1,18 @@
 'use strict';
 
-function move(from, to, player = undefined, pieces = undefined, visual = true) {
+function move(from, to, player = undefined, visual = true) {
     if (player == undefined) {
         player = currentPlayer;
     }
-    if (pieces == undefined) {
-        pieces = piecesArray;
-    }
-    let piece = findColoredPieceByPos(pieces[player], from);
-    let opponentPiece = findColoredPieceByPos(pieces[reverseColor[player]], to);
+    let piece = findColoredPieceByPos(piecesArray[player], from);
+    let opponentPiece = findColoredPieceByPos(piecesArray[reverseColor[player]], to);
     // if there is already a piece where the player places it's own one : its a capture
     if (opponentPiece != false) {
-        let newOpponentPieces = removePieceFromPos(pieces[reverseColor[currentPlayer]], to);
-        pieces[reverseColor[player]] = newOpponentPieces;
+        let newOpponentPieces = removePieceFromPos(piecesArray[reverseColor[currentPlayer]], to);
+        piecesArray[reverseColor[player]] = newOpponentPieces;
     }
     // check if the move is 'en passant'
-    enPassant(from, to, player, pieces, visual);
+    enPassant(from, to, player, visual);
     if (visual) {
         // check if the move is castling
         castle(from, to);
@@ -27,37 +24,34 @@ function move(from, to, player = undefined, pieces = undefined, visual = true) {
         // change the player
         currentPlayer = reverseColor[currentPlayer];
         // remove en passant
-        removeEnPassant(currentPlayer, pieces);
+        removeEnPassant(currentPlayer);
     }
     
 }
 
-function enPassant(from, to, player = undefined, pieces = undefined, visual = true) {
+function enPassant(from, to, player = undefined, visual = true) {
     if (player == undefined) {
         player = currentPlayer;
     }
-    if (pieces == undefined) {
-        pieces = piecesArray;
-    }
-    let piece = findColoredPieceByPos(pieces[player], from);
-    let opponentPiece = findColoredPieceByPos(pieces[reverseColor[player]], to);
+    let piece = findColoredPieceByPos(piecesArray[player], from);
+    let opponentPiece = findColoredPieceByPos(piecesArray[reverseColor[player]], to);
     // determine if its an 'en passant' move
     if ((delta(from, to) == 11 || delta(from, to) == 9)
         && piece.getType() == 'pawn'
         && opponentPiece == false) {
 
         // find the real position of the ennemy piece
-        opponentPiece = findColoredPieceByPos(pieces[reverseColor[player]], piece.getPos()-1);
+        opponentPiece = findColoredPieceByPos(piecesArray[reverseColor[player]], piece.getPos()-1);
         if (opponentPiece == false || opponentPiece.getEnPassant() == false) {
-            opponentPiece = findColoredPieceByPos(pieces[reverseColor[player]], piece.getPos()+1);
+            opponentPiece = findColoredPieceByPos(piecesArray[reverseColor[player]], piece.getPos()+1);
         }
         // If the piece can't be found, its not an 'en passant' move : just a capture
         if (opponentPiece == false || opponentPiece.getEnPassant() == false) {
             return;
         }
         // remove the piece
-        let newOpponentPieces = removePieceFromPos(pieces[reverseColor[player]], opponentPiece.getPos());
-        pieces[reverseColor[player]] = newOpponentPieces;
+        let newOpponentPieces = removePieceFromPos(piecesArray[reverseColor[player]], opponentPiece.getPos());
+        piecesArray[reverseColor[player]] = newOpponentPieces;
         // remove the piece from the visual board
         if (visual) {
             removeImageFromPos(opponentPiece.getPos());
@@ -112,21 +106,26 @@ function castle(from, to) {
 // if current player is white and his king is under attack
 //  -> remove all possibles moves if after it, the king is still under attack
 function removeMovesIfCheck(from, moves, color) {
+    let keep = {
+        'white': copyPiecesArray(piecesArray['white']),
+        'black': copyPiecesArray(piecesArray['black'])
+    }
     let newMoves = [];
     for (let to of moves) {
-        let pieces = simulate(from, to, color);
-        if (isCheck(color, pieces) == false) {
+        piecesArray['white'] = keep['white'];
+        piecesArray['black'] = keep['black'];
+        simulate(from, to, color);
+        if (isCheck(color) == false) {
             newMoves.push(to);
         }
     }
+    piecesArray['white'] = keep['white'];
+    piecesArray['black'] = keep['black'];
     return newMoves;
 }
 
 function simulate(from, to, color) {
-    let pieces = {
-        'white': copyPiecesArray(piecesArray['white']),
-        'black': copyPiecesArray(piecesArray['black'])
-    }
-    move(from, to, color, pieces, false);
-    return pieces;
+    piecesArray['white'] = copyPiecesArray(piecesArray['white']);
+    piecesArray['black'] = copyPiecesArray(piecesArray['black']);
+    move(from, to, color, false);
 }
